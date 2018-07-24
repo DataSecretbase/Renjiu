d3178f60a1f2dfd2e137ae4a6a106dbdec957908
developertqw2017 <2144799613@qq.com>
2018-07-24 19:41:40 +0800

    views apis

--- backend/jiaxiao/views_api.py
+++ backend/jiaxiao/views_api.py
@@ -65,4 +65,175 @@ class AccountViewSet(viewsets.ModelViewSet):
 							Q(major__icontains = params)|
 							Q(first_name__icontains = params)
 							)
+	
+	if self.kwargs.get('pk') is None:
+		return User.objects.all()
+	user_id = int(self.kwargs['pk'])
+	if self.request.user.id != user_id:
+		target_uesr = User.objects.get(id = user_id)
+		action = ViewedProfileTracking(actor = self.request.user, visited_profile = target_user)
+		action.save()
+	return User.objects.all()
 
+	def get_permissions(self):
+		if self.request.method in permission.SAFE_METHODS:
+			return (permission.AllowAny(),)
+		
+		if self.request.method == 'POST':
+			return (permission.AllowAny(),)
+
+		return (permission.IsAuthenticated(), IsAccountOwner(),)
+
+	@method_decorator(cache_page(10))
+	def dispatch(self, request):
+		recent_users = User.objcets.all().order_by('-last_login')
+		page = self.paginate_query(recent_users)
+
+		if page is not None:
+			serializer = self.get_serializer(page, context={'request',request}, many = True)
+			return self.get_paginated_response(serializer, data)
+
+		serializer = self.get_serializer(recent_users, context={'request':request}, many = True)
+		return Response(serializer.data)
+
+	@list_route()
+	def me(self, request, *arg, **kwargs):
+		if request.auth is None:
+			return Response({
+				'status':'Unauthorized',
+				'message': 'This authentication has been disabled.'
+			}, status = status.HTTP_401_UNAUTHORIZED)
+		user = User.objects.get(id = request.auth.user.id)
+
+		serializer = self.get_serializer(user, context={'request':request})
+		return Response(serializer.data)
+	
+	@list_route()
+	def who_visited_my_profile(self, request, *args, **kwars):
+		if request.auth is None:
+			return Response({
+				"status":"Unauthorized",
+			"message":"This authentication has been diable."
+
+				}, status = status.HTTP_401_UNAUTHORIZED)
+		response = ViewedProfileTracking.objects.filter(visited_profile = request.auth.user).order_by("-visited_time")
+		if not len(response):
+			return Response({}, status = status.HTTP_204_NO_CONTENT)
+
+		page = self.paginate_queryset(response)
+		if page is not None:
+			serializer = ViewdProfileTrackingSerializer(response, many = True)
+			return self.get_paginated_response(serializer.data)
+		serializer = ViewedProfileTrackingSerializer(response,many = True)
+		return Response(serializer.data)
+
+	def create(self, request):
+		serializer = self.serializer_class(data = request.data)
+		if serializer.is_valid():
+			if serializer.checkPassword(serializer.validated_data):
+				User.objects.create_user(**serializer.validated_data)
+				account = authenticate(username=serializer.validated_data['username'],
+							password=serializer.validated_data['password'])
+				login(request, account)
+				token = Token.objects.get(user=account)
+				serialized = self.serializer_class(account, context={'request': request})
+				return Response({
+					'account': serialized.data,
+					"token": token.key
+				}, status=status.HTTP_201_CREATED)
+		return Response({
+			'status': 'Bad request',
+			'message': 'User could not be created with received data.',
+			'errors': serializer.errors
+		}, status=status.HTTP_400_BAD_REQUEST)
+
+	@list_route(methods=['post'])
+	def register(self, request):
+		return self.create(request)
+
+	def destroy(self, request, pk=None):
+		account = self.get_object()
+		account.email = str(account.id)+'_nonactive'+'@gmail.com'
+		account.first_name = ''
+		account.last_name = ''
+		account.is_active = False
+		account.is_authenticated = False
+		account.save()
+		
+		logout(request)
+
+		# return self.delete(request)
+		return Response({'status': 'deleted clean'})
+
+	@detail_route(methods=['put'])
+	def update_user(self, request, pk=None):
+		account = self.get_object()
+		serializer = self.serializer_class(account, data=request.data, partial=True)
+		if serializer.is_valid(raise_exception=True):
+			serializer.update(account, serializer.validated_data)
+			update_session_auth_hash(request, account)
+			return Response({"status": "Updated!"})
+		else:
+			return Response(serializer.errors,
+					status=status.HTTP_400_BAD_REQUEST)
+
+	@detail_route(methods=['patch'])
+	def set_password(self, request, pk=1):
+		account = self.get_object()
+		serializer = self.serializer_class(account, data=request.data, partial=True)
+		if serializer.is_valid(raise_exception=True):
+			serializer.update(account, serializer.validated_data)
+			update_session_auth_hash(request, account)
+			return Response({'status': 'password set'})
+		else:
+			return Response(serializer.errors,
+					status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class LoginView(views.APIView):
+	permission_classes = (permissions.AllowAny,)
+
+	def post(self, request, format=None):
+
+		username = request.data.get('username', None)
+		password = request.data.get('password', None)
+
+		account = authenticate(username=username, password=password)
+
+		if account is not None:
+			if account.is_active:
+				login(request, account)
+				token, _ = Token.objects.get_or_create(user=account)
+				serialized = AccountSerializer(account, context={'request': request})
+				return Response({
+					'account': serialized.data,
+					"token": token.key
+				})
+			else:
+				return Response({
+					'status': 'Unauthorized',
+					'message': 'This authentication has been disabled.'
+				}, status=status.HTTP_401_UNAUTHORIZED)
+		else:
+			if request.user.is_active:
+				token, _ = Token.objects.get_or_create(user=account)
+				serialized = AccountSerializer(request.user, context={'request': request})
+				return Response({
+					'account': serialized.data,
+					'token': token.key
+				})
+			return Response({
+				'status': 'Unauthorized',
+				'message': 'Username/password combination invalid.'
+			}, status=status.HTTP_401_UNAUTHORIZED)
+
+
+class LogoutView(views.APIView):
+	permission_classes = (permissions.IsAuthenticated,)
+
+	def post(self, request, format=None):
+		request.auth_token.delete()
+		logout(request)
+
+		return Response({}, status=status.HTTP_204_NO_CONTENT)
