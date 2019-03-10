9a2db8128b66becf742ce6e31407d61792630b67
developertqw2017 <2144799613@qq.com>
2019-03-10 17:34:01 +0800

    init

--- leagueOfDrivers_BE/wx_league/modelss/base.py
+++ leagueOfDrivers_BE/wx_league/modelss/base.py
@@ -50,39 +50,38 @@ class WechatUser(AbstractUser):
         return self.country
 
 
-class DriverSchool(models.Model):
-    province_id = models.IntegerField(verbose_name='省', default=0)
-    # PROVINCE = ((0,'beijin'),)
-    # CITY = ((0,'beijin'),(1,"shanghai"))
-    city_id = models.IntegerField(verbose_name='城市', default=0)
-    # DISTRICT = ((0,'beijin'))
-    district_id = models.IntegerField(verbose_name='区', default=0)
-    name = models.CharField(verbose_name='店铺名称', max_length=30)
+class ShopPosition(models.Model):
+    province = models.IntegerField(verbose_name='省', default=0)
+    city = models.IntegerField(verbose_name='城市', default=0)
+    district = models.IntegerField(verbose_name='区', default=0)
     address = models.CharField(verbose_name='地址',
                                max_length=100,
                                blank=True,
                                null=True)
+    latitude = models.FloatField(verbose_name='纬度')
+    longitude = models.FloatField(verbose_name='经度')
+    shop = models.ForeignKey(Shop)
+
+
+class Shop(models.Model):
+    name = models.CharField(verbose_name='店铺名称', max_length=30)
     phone = models.CharField(verbose_name='联系电话',
                              max_length=50,
                              blank=True,
                              null=True)
-    introduce = models.TextField(verbose_name='驾校介绍')
-    characteristic = models.TextField(verbose_name='驾校特色')
-    sort = models.IntegerField(verbose_name='排序')
+    introduce = models.TextField(verbose_name='介绍')
+    characteristic = models.TextField(verbose_name='特色')
     pic = models.ForeignKey('Icon',
-                            verbose_name='驾校商标',
+                            verbose_name='商标',
                             on_delete=models.DO_NOTHING)
     activity = models.CharField(verbose_name='打折优惠信息', max_length=255)
-    latitude = models.FloatField(verbose_name='纬度')
-    longitude = models.FloatField(verbose_name='经度')
     number_good_reputation = models.IntegerField(verbose_name='好评数')
     number_order = models.IntegerField(verbose_name='订单数')
 
     class Meta:
-        db_table = 'DriverSchool'
-        verbose_name = '驾校'
-        verbose_name_plural = '驾校'
-        _order = 'sort'
+        db_table = 'Shop'
+        verbose_name = '店铺'
+        verbose_name_plural = '店铺'
 
     def __str__(self):
         return self.name
